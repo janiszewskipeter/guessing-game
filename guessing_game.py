56844fb56437ceaa750d0f1b11b5911f685bbf6e
Piotr Janiszewski <janiszewskipeter@gmail.com>
2020-03-23 16:19:45 +0100

    2.Variable names are meaningful

--- guessing_game.py
+++ guessing_game.py
@@ -1,53 +1,53 @@
-# program gives a random number to guess by the user, if guess is lower or higher than the number
-# program shows a message 
+# program gives new_empty_list random number to guess by the user, if guess is lower or higher than the number
+# program shows new_empty_list message 
 import math # import match module   
 import random # import random module
 
 def main():
-    a = [] #initialize empty list
-    a.append(random.randint(1, 99))# adds 1st random number to [a] list
-    a.append(random.randint(1, 99))# adds 2nd random number to [a] list
-    a.append(random.randint(1, 99))# adds 3th random number to [a] list
-    a.append(random.randint(1, 99))# adds 4th random number to [a] list
-    a.append(random.randint(1, 99))# adds 5th random number to [a] list
-    a.append(random.randint(1, 99))# adds 6th random number to [a] list
-    a.append(random.randint(1, 99))# adds 7th random number to [a] list
-    a.append(random.randint(1, 99))# adds 8th random number to [a] list
-    a.append(random.randint(1, 99))# adds 9th random number to [a] list
-    a.append(random.randint(1, 99))# adds 10th random number to [a] list
+    new_empty_list = [] #initialize empty list
+    new_empty_list.append(random.randint(1, 99))# adds 1st random number to [new_empty_list] list
+    new_empty_list.append(random.randint(1, 99))# adds 2nd random number to [new_empty_list] list
+    new_empty_list.append(random.randint(1, 99))# adds 3th random number to [new_empty_list] list
+    new_empty_list.append(random.randint(1, 99))# adds 4th random number to [new_empty_list] list
+    new_empty_list.append(random.randint(1, 99))# adds 5th random number to [new_empty_list] list
+    new_empty_list.append(random.randint(1, 99))# adds 6th random number to [new_empty_list] list
+    new_empty_list.append(random.randint(1, 99))# adds 7th random number to [new_empty_list] list
+    new_empty_list.append(random.randint(1, 99))# adds 8th random number to [new_empty_list] list
+    new_empty_list.append(random.randint(1, 99))# adds 9th random number to [new_empty_list] list
+    new_empty_list.append(random.randint(1, 99))# adds 10th random number to [new_empty_list] list
     for i in range(10): # loop for i in range 1 -10
-        g = int(input("Enter an integer from 1 to 99: ")) # takes an integer input and creates a variable g
-        while a[i] != g: # while firs number of a list is not even with input g do:
-            if g < a[i]: # if user input lover than first index of the list 
+        user_guess = int(input("Enter an integer from 1 to 99: ")) # takes an integer input and creates new_empty_list variable user_guess
+        while new_empty_list[i] != user_guess: # while firs number of new_empty_list list is not even with input user_guess do:
+            if user_guess < new_empty_list[i]: # if user input lover than first index of the list 
                 print("guess is low") # print text
-                g = int(input("Enter an integer from 1 to 99: "))# takes an integer input and creates a variable g
-            elif g > a[i]: #else if user input higher than first etc element of the list
+                user_guess = int(input("Enter an integer from 1 to 99: "))# takes an integer input and creates new_empty_list variable user_guess
+            elif user_guess > new_empty_list[i]: #else if user input higher than first etc element of the list
                 print("guess is high") #print text
-                g = int(input("Enter an integer from 1 to 99: "))# takes an integer input and creates a variable g
+                user_guess = int(input("Enter an integer from 1 to 99: "))# takes an integer input and creates new_empty_list variable user_guess
             else: # any other input will:
                 break #end loop 
         print("you guessed it!") #print text
 
-    b = [] #initialize empty list
-    b.append(random.randint(1, 49))# adds 1st random number to [b] list
-    b.append(random.randint(1, 49))# adds 2nd random number to [b] list
-    b.append(random.randint(1, 49))# adds 3th random number to [b] list
-    b.append(random.randint(1, 49))# adds 4th random number to [b] list
-    b.append(random.randint(1, 49))# adds 5th random number to [b] list
-    b.append(random.randint(1, 49))# adds 6th random number to [b] list
-    b.append(random.randint(1, 49))# adds 7th random number to [b] list
-    b.append(random.randint(1, 49))# adds 8th random number to [b] list
-    b.append(random.randint(1, 49))# adds 9th random number to [b] list
-    b.append(random.randint(1, 49))# adds 10th random number to [b] list
-    for i in range(10):# loop for i in range 1 -10
-        g = int(input("Enter an integer from 1 to 49: ")) # takes an integer input and creates a variable g
-        while b[i] != g:# while firs number of a list is not even with input g do:
-            if g < b[i]:# if user input lover than first index of the list 
+    new_empty_list2 = [] #initialize empty list
+    new_empty_list2.append(random.randint(1, 49))# adds 1st random number to [new_empty_list2] list
+    new_empty_list2.append(random.randint(1, 49))# adds 2nd random number to [new_empty_list2] list
+    new_empty_list2.append(random.randint(1, 49))# adds 3th random number to [new_empty_list2] list
+    new_empty_list2.append(random.randint(1, 49))# adds 4th random number to [new_empty_list2] list
+    new_empty_list2.append(random.randint(1, 49))# adds 5th random number to [new_empty_list2] list
+    new_empty_list2.append(random.randint(1, 49))# adds 6th random number to [new_empty_list2] list
+    new_empty_list2.append(random.randint(1, 49))# adds 7th random number to [new_empty_list2] list
+    new_empty_list2.append(random.randint(1, 49))# adds 8th random number to [new_empty_list2] list
+    new_empty_list2.append(random.randint(1, 49))# adds 9th random number to [new_empty_list2] list
+    new_empty_list2.append(random.randint(1, 49))# adds 10th random number to [new_empty_list2] list
+    for each_number_in_the_list in range(10):# loop for i in range 1 -10
+        user_guess = int(input("Enter an integer from 1 to 49: ")) # takes an integer input and creates new_empty_list variable user_guess
+        while new_empty_list2[each_number_in_the_list] != user_guess:# while firs number of new_empty_list list is not even with input user_guess do:
+            if user_guess < new_empty_list2[i]:# if user input lover than first index of the list 
                 print("guess is low")#print text
-                g = int(input("Enter an integer from 1 to 49: "))# takes an integer input and creates a variable g
-            elif g > b[i]:#else if user input higher than first etc element of the list
+                user_guess = int(input("Enter an integer from 1 to 49: "))# takes an integer input and creates new_empty_list variable user_guess
+            elif user_guess > new_empty_list2[each_number_in_the_list]:#else if user input higher than first etc element of the list
                 print("guess is high")#print text
-                g = int(input("Enter an integer from 1 to 49: "))# takes an integer input and creates a variable g
+                user_guess = int(input("Enter an integer from 1 to 49: "))# takes an integer input and creates new_empty_list variable user_guess
             else: # other way than if or elif
                 break#end loop 
         print("you guessed it!")#print text
